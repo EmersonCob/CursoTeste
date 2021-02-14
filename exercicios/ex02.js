57ecaa12453d1754ebb4375f7fbc6c9d8c304a30
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-02-14 19:54:23 -0300

    Update
    new exercise with function, function  anonymous and arrow function.

---
+++ exercicios/ex02.js
@@ -0,0 +1,52 @@
+// Functions
+
+function operacao(number1, number2){
+    console.log(number1 * number2)
+}
+
+operacao(12, 10)
+
+// Function anonima dentro de uma variável
+
+let sum = function(number1, number2){
+    let total = number1 - number2
+    return total
+}
+
+let n1 = 2021
+let n2 = 25
+
+console.log(`Você nasceu em: ${sum(n1, n2)}`)
+
+// Function scope exemplo 1
+
+
+let subject
+
+function createThink(subject){
+   return subject
+}
+console.log(createThink(subject))
+
+
+// Function scope exemplo 2
+
+
+let subject1 = 'create video'
+
+function createThink1(subject1){ // ao inserir o subject como pârametro,  
+    subject1 = 'study'         // estou criando um novo subject dentro da função
+    return subject1          // deixando o subject de fora do escopo com o valor inicial
+}
+
+console.log(createThink1(subject1))
+console.log(subject1)
+
+// arrow function
+
+
+const sayMyname = () => {
+    console.log("Mayk")
+}
+
+sayMyname()
