e14d39cb8c207a86df5db2466d55daab17217f66
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-02-16 15:25:07 -0300

    Update modeloEx16.js
    Comentários

--- AtividadeJS/modeloEx16.js
+++ AtividadeJS/modeloEx16.js
@@ -1,9 +1,9 @@
-let num = document.querySelector('input#fnum')
-let lista = document.querySelector('select#flista')
-let res = document.querySelector('div#res')
-let valores = []
+let num = document.querySelector('input#fnum') // recebe o valor inserido no imput pelo usuario
+let lista = document.querySelector('select#flista') // exibe o valor inserido no imput apos a função chamar a adicionar()
+let res = document.querySelector('div#res') // exibe os valores da função finalizar().
+let valores = [] // armazena no vetor valores, os números inseridos em num.
 
-function isNumero(n){
+function isNumero(n){ // Essa função se encarrega de controlar a entreda de números iguais e maiores que 1 e menores ou iguais a 100.
     if(Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
@@ -11,29 +11,29 @@ function isNumero(n){
     }
 }
 
-function inLista(n, l){
-    if(l.indexOf(Number(n)) != -1) {
-        return true
+function inLista(n, l){ // Essa função verifica se um determinado número está na lista e retorna verdadeiro ou falso conforme o resultado.
+    if(l.indexOf(Number(n)) != -1) { // Se na lista de indexOf o Number de (n) for diferente de -1, o valor está na lista caso contrario não.
+        return true                    // Quando o retorno for -1 indica que o valor de (n) não foi encontrado da lista.
     } else {
         return false
     }
-}
+}   
 
-function adicionar(){
-    if(isNumero(num.value) && !inLista(num.value, valores)){
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
-        lista.appendChild(item)
+function adicionar(){ // Função para receber e tratar os valores
+    if(isNumero(num.value) && !inLista(num.value, valores)){ // Só vai adicionar se os dois resultados forem true.
+        valores.push(Number(num.value)) // Armazena o elemento no vetor usando o metodo .push
+        let item = document.createElement('option') // cria um elemento e adiciona os elementos em uma tag no caso o select. 
+        item.text = `Valor ${num.value} adicionado.` // tranforma o item em uma String com posibilidade de interpolação e adiciona na lista.
+        lista.appendChild(item) 
         res.innerHTML = ''
     } else {
         window.alert('Valor invalido ou já encontrado na lista.')
     }
-    num.value = ''
-    num.focus()
+    num.value = '' // recebe o valor vazio para manter o imput limpo para o usuario
+    num.focus() // da foco no imput com o cursor do mouse
 }
 
-function finalizar() {
+function finalizar() { // função para o botão finalizar com todos os dados finais.
     if(valores.length == 0) {
         window.alert('Adicione valores antes de finalizar!')
     } else {
